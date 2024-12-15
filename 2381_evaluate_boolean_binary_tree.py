7d079636b5c3b8f255782432e7b046b7a3afc29b
Andrew Chow <ac.andrewchow@gmail.com>
2024-12-15 16:02:09 -0500

    iterative approach

--- 2381_evaluate_boolean_binary_tree.py
+++ 2381_evaluate_boolean_binary_tree.py
@@ -17,6 +17,27 @@ class Solution:
         elif root.val == 3:  # AND 
             return left_result and right_result
 
+    # def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+    #     stack = [root]
+    #     value = {} # map node -> value
+
+    #     while stack:
+    #         node = stack.pop()
+    #         # leaf node
+    #         if not node.left:
+    #             value[node] = node.val == 1
+    #         # non-leaf node
+    #         else:
+    #             if node.left in value:
+    #                 if node.val == 2:
+    #                     value[node] = value[node.left] or value[node.right]
+    #                 if node.val == 3:
+    #                      value[node] = value[node.left] and value[node.right]
+    #             # otherwise add children
+    #             else:
+    #                 stack.extend([node, node.left, node.right])
+    #     return value[root]
+
         
     
 
